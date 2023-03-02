76f7391d2c25dd743b9fe470da4eb4a3a96f880b
sot26 <omotoshotobi26@gmail.com>
2023-03-02 22:10:22 +0100

    creating auth page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Reset from './pages/auth/Reset';
 import Contact from './pages/contact/Contact';
 import Home from './pages/home/Home';
 
@@ -14,6 +17,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/reset' element={<Reset />} />
         </Routes>
       <Footer />
       </BrowserRouter>
