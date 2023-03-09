e4dc602bc7a5f37b9e89814143df32c745768494
sot26 <omotoshotobi26@gmail.com>
2023-03-09 22:30:34 +0100

    creating a slider

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Slider from "../../components/slider/Slider";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Slider />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
