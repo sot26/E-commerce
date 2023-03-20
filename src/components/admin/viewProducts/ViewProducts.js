d65dc3e6f0e1b050ef32060056703ff6a21f657b
sot26 <omotoshotobi26@gmail.com>
2023-03-20 19:14:21 +0100

    get product from firestore

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,6 +1,32 @@
-import React from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { db } from "../../../firebase/config";
 
 const ViewProducts = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  const getProduct = () => {
+    try {
+      const productsRef = collection(db, "products");
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (querySnapshot) => {
+        const allProduct = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setProduct(allProduct);
+        console.log(allProduct);
+      });
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return <div>ViewProducts</div>;
 };
 
