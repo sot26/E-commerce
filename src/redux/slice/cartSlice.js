e6842eec13ceb51d1015eac1cc67c753cede69e2
sot26 <omotoshotobi26@gmail.com>
2023-05-07 22:04:53 +0100

    add to cart

--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
   cartItems: localStorage.getItem("cartItems")
@@ -14,6 +15,25 @@ const cartSlice = createSlice({
   reducers: {
     ADD_TO_CART(state, action) {
       console.log(action.payload);
+      const productIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (productIndex >= 0) {
+        //item alraedy exist in the cart
+        //incrase the cart quantity
+        state.cartItems[productIndex].cartQuantity += 1;
+        toast.info(`${action.payload.name} increased by one`, {
+          position: "top-left",
+        });
+      } else {
+        const tempProduct = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(tempProduct);
+        toast.success(`${action.payload.name}  added to cart`, {
+          position: "top-left",
+        });
+      }
+      // save cart to ls
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
   },
 });
