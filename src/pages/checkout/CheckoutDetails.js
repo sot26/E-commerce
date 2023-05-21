5badc75decd30d888dc201fdede1a950f4625528
sot26 <omotoshotobi26@gmail.com>
2023-05-21 21:14:26 +0100

    shipping

--- src/pages/checkout/CheckoutDetails.js
+++ src/pages/checkout/CheckoutDetails.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialAddressState = {
+  name: "",
+  line1: "",
+  line2: "",
+  city: "",
+  state: "",
+  postal_code: "",
+  country: "",
+  phone: "",
+};
 
 const CheckoutDetails = () => {
+  const [shippingAddress, setShippingAddress] = useState({
+    ...initialAddressState,
+  });
+  const [billingAddress, setBillingAddress] = useState({
+    ...initialAddressState,
+  });
+
+  const handleShipping = () => {};
+  const handleBilling = () => {};
   return <div>CheckoutDetails</div>;
 };
 
