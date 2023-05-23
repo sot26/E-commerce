ab02b0368188d994cbf3a014bfb0f4a501e6fa41
sot26 <omotoshotobi26@gmail.com>
2023-05-23 22:26:45 +0100

    checkoutDetails

--- src/pages/checkout/CheckoutDetails.js
+++ src/pages/checkout/CheckoutDetails.js
@@ -6,6 +6,7 @@ import {
 import { useSelector } from "react-redux";
 import { selectCartItems } from "../../redux/slice/cartSlice";
 import { Link } from "react-router-dom";
+import { CountryDropdown } from "react-country-region-selector";
 
 const initialAddressState = {
   name: "",
@@ -30,28 +31,251 @@ const CheckoutDetails = () => {
   const cartTotalQuantity = useSelector(selectCartTotalQuantity);
   const cartItems = useSelector(selectCartItems);
 
-  const handleShipping = () => {};
-  const handleBilling = () => {};
+  const handleShipping = (e) => {
+    const { name, value } = e.target;
+    setShippingAddress({
+      ...shippingAddress,
+      [name]: value,
+    });
+  };
+  const handleBilling = (e) => {
+    const { name, value } = e.target;
+    setBillingAddress({
+      ...billingAddress,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(shippingAddress);
+    console.log(billingAddress);
+  };
   return (
     <div className="w-full">
-      <div className="w-full px-20">
-        <p className="text-4xl font-semibold">Checkout Details</p>
-        <div className="flex">
-          <div className="w-[65%]">
-            <p>Shipping Address</p>
-            <form>
-              <div>
-                <p className="text-xl font-semibold">Recipient Name:</p>
-                <input
-                  className="border-[2px] border-black p-2 "
-                  type="text"
-                  placeholder=""
-                  required
-                />
-              </div>
-            </form>
+      <div className="w-full px-20 py-12">
+        <p className="text-4xl font-semibold pb-4">Checkout Details</p>
+        <div className="md:flex">
+          <div className="w-[55%] shadow-2xl p-3">
+            <div className="">
+              <p className="text-3xl ">Shipping Address</p>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <p className="text-lg font-bold my-2">Recipient Name:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Recipient name"
+                    value={shippingAddress.name}
+                    name="name"
+                    onChange={(e) => handleShipping(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Address line 1:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Address line 1"
+                    value={shippingAddress.line1}
+                    name="line1"
+                    onChange={(e) => handleShipping(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Address line 2:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Address line 2"
+                    value={shippingAddress.line2}
+                    name="line2"
+                    onChange={(e) => handleShipping(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">City:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="City"
+                    value={shippingAddress.city}
+                    name="city"
+                    onChange={(e) => handleShipping(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">State:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="State"
+                    value={shippingAddress.state}
+                    name="state"
+                    onChange={(e) => handleShipping(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Postal Code:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Postal Code"
+                    value={shippingAddress.postal_code}
+                    name="postal_code"
+                    onChange={(e) => handleShipping(e.target.value)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Country:</p>
+                  <CountryDropdown
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    valueType="short"
+                    value={shippingAddress.country}
+                    onChange={(val) =>
+                      handleShipping({
+                        target: {
+                          name: "country",
+                          value: val,
+                        },
+                      })
+                    }
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Phone:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Phone"
+                    value={shippingAddress.phone}
+                    name="phone"
+                    onChange={(e) => handleShipping(e)}
+                    required
+                  />
+                </div>
+              </form>
+            </div>
+
+            {/* Billing address */}
+            <div className=" mt-12">
+              <p className="text-3xl ">Billing Address</p>
+              <form onSubmit={handleBilling}>
+                <div>
+                  <p className="text-lg font-bold my-2">Recipient Name:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Recipient name"
+                    value={billingAddress.name}
+                    name="name"
+                    onChange={(e) => handleBilling(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Address line 1:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Address line 1"
+                    value={billingAddress.line1}
+                    name="line1"
+                    onChange={(e) => handleBilling(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Address line 2:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Address line 2"
+                    value={billingAddress.line2}
+                    name="line2"
+                    onChange={(e) => handleBilling(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">City:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="City"
+                    value={billingAddress.city}
+                    name="city"
+                    onChange={(e) => handleBilling(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">State:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="State"
+                    value={billingAddress.state}
+                    name="state"
+                    onChange={(e) => handleBilling(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Postal Code:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Postal Code"
+                    value={billingAddress.postal_code}
+                    name="postal_code"
+                    onChange={(e) => handleBilling(e)}
+                    required
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Country:</p>
+                  <CountryDropdown
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    valueType="short"
+                    value={billingAddress.country}
+                    onChange={(val) =>
+                      handleBilling({
+                        target: {
+                          name: "country",
+                          value: val,
+                        },
+                      })
+                    }
+                  />
+                </div>
+                <div>
+                  <p className="text-lg font-bold my-2">Phone:</p>
+                  <input
+                    className="border-[1.5px] border-black p-2 rounded-md w-full"
+                    type="text"
+                    placeholder="Phone"
+                    value={billingAddress.phone}
+                    name="phone"
+                    onChange={(e) => handleBilling(e)}
+                    required
+                  />
+                  <button
+                    type="submit"
+                    className="p-2 mt-3 text-white text-md md:text-lg bg-blue-600 rounded-lg"
+                  >
+                    Proceed To Checkout
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-          <div className="w-[35%]">
+          <div className="w-[45%]">
             {cartItems.length === 0 ? (
               <div>
                 <p className="text-2xl">No item in your cart</p>
